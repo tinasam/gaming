49542d73a62f2a403ecf10aa425cd417612864a4
Tina Samuels <mssamuelstina@gmail.com>
2022-10-03 18:12:02 -0400

    added first two shapes

--- game.js
+++ game.js
@@ -1,34 +1,8 @@
-// Draw a circle
-const drawCircle = () => {
-    const cirlce = document.getElementById("canvas");
-    const ctx = cirlce.getContext("2d");
-    ctx.beginPath();
-    ctx.arc(100, 75, 50, 0, 2 * Math.PI);
-    ctx.stroke();
-}
+// insert a blue circle
+// const firstCircle = document.querySelector(.circle)
 
 // Draw a Rectangle
-const drawRectangle = () => {
-    const rectangle = document.getElementById("canvas");
-    if (rectangle.getContext) {
-        const context = rectangle.getContext("2d");
 
-        context.fillRect(20, 20, 100, 100);
-        context.clearRect(30, 30, 80, 80);
-        ctx.strokRect(50, 50, 50, 50);
-    }
-}
 
 // Draw a Triangle
-const drawTriangle = () => {
-    const triangle = document.getElementById("canvas");
-    if (triangle.getContext) {
-        const context = triangle.getContext("2d");
 
-        context.beginPath();
-        context.moveTo(75, 80);
-        context.lineTo(10,75);
-        context.lineTo(10,25);
-        context.fill();
-    }
-}
