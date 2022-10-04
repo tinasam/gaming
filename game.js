c0769d3b5029bbbac881f0721946a7c0b8d9251b
Tina Samuels <mssamuelstina@gmail.com>
2022-10-04 15:34:23 -0400

    encapsulated divs inside div board for card game

--- game.js
+++ game.js
@@ -1,8 +1,19 @@
-// insert a blue circle
-// const firstCircle = document.querySelector(.circle)
+// Baby matching game 
+    //Click first card to reveal shape/image
+    //Click second card to see if it matches
+        //if the two cards match give one point and return "Woo-hoo!"
+        //if the cards do not match return "Try Again"
+    
+    //Game ends in a "WIN"
+        // when all uncovered cards match & points ==6
+        // OR
+        // 1 minute expires
 
-// Draw a Rectangle
+const firstCard;
+const secondCard;
 
 
-// Draw a Triangle
+
+
+
 
