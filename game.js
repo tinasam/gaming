36b5f2f891406002ad4e1b9093314f1a65682fa2
Tina Samuels <mssamuelstina@gmail.com>
2022-10-05 00:58:19 -0400

    Began *.js

--- game.js
+++ game.js
@@ -7,11 +7,53 @@
     //Game ends in a "WIN"
         // when all uncovered cards match & points ==6
         // OR
-        // 1 minute expires
+        // after 1 minute expires return "Sorry game over"
 
-const firstCard;
-const secondCard;
+let points = 0;
+const cardList = document.querySelectorAll([(".grid-item")]);
 
+
+let startButton = document.querySelector ("#onOff");
+console.log(cardList);
+
+let cardSet;
+let grid = [];
+
+let shuffleCards = () => {
+    cardSet = cardList.concat(cardList);  // This displays two of each card
+    console.log(cardSet);
+}
+
+window.onload = function() {
+    shuffleCards();
+    startGame();
+}
+
+// let shuffleCards = () => {
+//     cardSet = cardList.concat(cardList);  // This displays two of each card
+//     console.log(cardSet);
+// }
+
+
+
+    //shuffle
+    // for (let i = 0; i < cardSet.length; i++) {
+    //     let j = Math.floor(Math.random() * cardSet.length); //gets random i
+    //     //swap
+    //     let temp = cardSet[i];
+    //     cardSet[i] = cardSet[j];
+    //     cardSet[j] = temp;
+    // }
+    // console.log(cardSet);
+// }
+
+// const startGame = () => {
+
+// }
+
+startButton.addEventListener("click", (event) => {
+    startOver();
+});
 
 
 
