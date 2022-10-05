6e7d4b58481d1c01b2b9a9bec30e020e491a95ec
Tina Samuels <mssamuelstina@gmail.com>
2022-10-05 15:15:11 -0400

    Created eventlistener to flip cards

--- game.js
+++ game.js
@@ -10,41 +10,48 @@
         // after 1 minute expires return "Sorry game over"
 
 let points = 0;
-const cardList = document.querySelectorAll([(".grid-item")]);
+// Cards display
+const cardList = document.querySelectorAll(".grid-item");
+const startButton = document.querySelector ("#onOff");
+const frontSideCard = document.querySelectorAll(".backgroundImg");
+
+// Click to reveal shapes and images
+frontSideCard.addEventListener("click", (event) => {
+    frontSideCard.style.visibility="hidden";
+    cardList.style.visibility="visible";
+});
+
+cardList.addEventListener("click", (event) => {
+    cardList.style.visibility="hidden";
+    frontSideCard.style.visibility="visible";
+});
+
+// cardList.forEach((card) => {
+
+// }
 
 
-let startButton = document.querySelector ("#onOff");
-console.log(cardList);
+// let cardSet;
+// empty array to display cards 
 
-let cardSet;
-let grid = [];
 
-let shuffleCards = () => {
-    cardSet = cardList.concat(cardList);  // This displays two of each card
-    console.log(cardSet);
-}
-
-window.onload = function() {
-    shuffleCards();
-    startGame();
-}
+// window.onload = function() {
+//     shuffleCards();
+//     startGame();
+// }
 
 // let shuffleCards = () => {
 //     cardSet = cardList.concat(cardList);  // This displays two of each card
 //     console.log(cardSet);
-// }
-
-
-
-    //shuffle
-    // for (let i = 0; i < cardSet.length; i++) {
-    //     let j = Math.floor(Math.random() * cardSet.length); //gets random i
-    //     //swap
-    //     let temp = cardSet[i];
-    //     cardSet[i] = cardSet[j];
-    //     cardSet[j] = temp;
-    // }
-    // console.log(cardSet);
+//     // shuffle
+//     for (let i = 0; i < cardSet.length; i++) {
+//         let j = Math.floor(Math.random() * cardSet.length); //gets random i
+//         //swap
+//         let temp = cardSet[i];
+//         cardSet[i] = cardSet[j];
+//         cardSet[j] = temp;
+//     }
+//     console.log(cardSet);
 // }
 
 // const startGame = () => {
